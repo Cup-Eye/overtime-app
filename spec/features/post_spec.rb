9c836e6cd1d2b747d4a0fea00541984233d6a32c
Cup-Eye <mail@berkaykabay.de>
2020-02-13 16:21:05 +0100

    refactored the code for post creation

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it "can be reached successfully" do
+    it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
 
-    it "has a title of Posts" do
+    it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
-end
+  end
 
   describe 'creation' do
     before do
